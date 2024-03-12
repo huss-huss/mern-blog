a6c4dff200a39eddf32a81771095b7b9faff9fa8
huss-huss <husibest000@gamil.com>
2024-03-12 14:46:54 +0300

    create  and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
-import './App.css'
-import Home from './pages/Home'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComponent from "./components/Footer";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <FooterComponent />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
